407e6efd5ff825c3391d5ec38811e86cfcdd6c3c
Pavel-Baranenko <baranenko_work@mail.ru>
2023-07-22 10:49:02 +0300

    jk

--- assets/js/progressbar.js
+++ assets/js/progressbar.js
@@ -1,9 +1,8 @@
 function progressBar() {
   let scroll = document.body.scrollTop || document.documentElement.scrollTop;
   let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  let scrolled = scroll / height * 10;
-  let scroolHeight = 14.5;
-  document.getElementById('progressBar').style.width = (scroolHeight - scrolled) + '%';
+  let scrolled = scroll / height * 100;
+  document.getElementById('progressBar').style.marginLeft = (scrolled) + '%';
 }
 window.addEventListener('scroll', progressBar);
 
